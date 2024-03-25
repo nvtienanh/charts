c82c63fde5ac548748be3556dbd20fb1a2a29353
Anh Nguyen <nvtienanh@gmail.com>
2024-03-25 23:42:36 +0700

    Update

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
 
 class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -11,5 +10,3 @@ class CompressorRecipe(ConanFile):
     def build_requirements(self):
         self.tool_requires("cmake/3.22.6")
     
-    def layout(self):
-        cmake_layout(self)
